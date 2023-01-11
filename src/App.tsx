3d5dcb457decfab3a5bfffbdd0bd6c7d5e1caa36
Weslley Rodrigues <weslley.rdgs@gmail.com>
2023-01-11 20:00:02 -0300

    feat(mod02exec1): implementa estate aos inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
+import {useState, ChangeEvent} from 'react'
+
 const App = () => {
+
+  const [state, setState] = useState({
+    name: '',
+    lastName: '',
+    age: 0
+  })
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) =>{
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" name='name' value={state.name} onChange={handleChangeInput}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" name='lastName' value={state.lastName} onChange={handleChangeInput}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={state.age} 
+        onChange={(e)=>setState(
+          {...state, age: parseInt(e.target.value)}
+        )}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {`${state.name} ${state.lastName}`}, tudo bem?<br/>
+      Você tem {state.age} anos.
     </div>
   );
 }
-
 export default App;
